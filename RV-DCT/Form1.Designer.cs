af75e1e47d11aedfaa11a7c00986710455149db9
Andrej Knez <andrej.531.knez@gmail.com>
2017-01-04 00:39:20 +0100

    nic

--- RV-DCT/Form1.Designer.cs
+++ RV-DCT/Form1.Designer.cs
@@ -28,12 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.LoadBtn = new System.Windows.Forms.Button();
+            this.CompressBtn = new System.Windows.Forms.Button();
+            this.DecompressBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LoadBtn
+            // 
+            this.LoadBtn.Location = new System.Drawing.Point(565, 12);
+            this.LoadBtn.Name = "LoadBtn";
+            this.LoadBtn.Size = new System.Drawing.Size(86, 35);
+            this.LoadBtn.TabIndex = 0;
+            this.LoadBtn.Text = "Naloži sliko";
+            this.LoadBtn.UseVisualStyleBackColor = true;
+            this.LoadBtn.Click += new System.EventHandler(this.LoadBtn_Click);
+            // 
+            // CompressBtn
+            // 
+            this.CompressBtn.Location = new System.Drawing.Point(565, 53);
+            this.CompressBtn.Name = "CompressBtn";
+            this.CompressBtn.Size = new System.Drawing.Size(86, 35);
+            this.CompressBtn.TabIndex = 1;
+            this.CompressBtn.Text = "Kompresiraj sliko";
+            this.CompressBtn.UseVisualStyleBackColor = true;
+            this.CompressBtn.Click += new System.EventHandler(this.CompressBtn_Click);
+            // 
+            // DecompressBtn
+            // 
+            this.DecompressBtn.Location = new System.Drawing.Point(565, 94);
+            this.DecompressBtn.Name = "DecompressBtn";
+            this.DecompressBtn.Size = new System.Drawing.Size(86, 35);
+            this.DecompressBtn.TabIndex = 2;
+            this.DecompressBtn.Text = "Dekompresiraj sliko";
+            this.DecompressBtn.UseVisualStyleBackColor = true;
+            this.DecompressBtn.Click += new System.EventHandler(this.DecompressBtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(546, 400);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 424);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.DecompressBtn);
+            this.Controls.Add(this.CompressBtn);
+            this.Controls.Add(this.LoadBtn);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button LoadBtn;
+        private System.Windows.Forms.Button CompressBtn;
+        private System.Windows.Forms.Button DecompressBtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- RV-DCT/Form1.cs
+++ RV-DCT/Form1.cs
@@ -12,9 +12,74 @@ namespace RV_DCT
 {
     public partial class Form1 : Form
     {
+        Bitmap img;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void LoadBtn_Click(object sender, EventArgs e)
+        {
+
+            Bitmap img = null;
+            int width=0, height=0;
+            OpenFileDialog open = new OpenFileDialog();
+            if(open.ShowDialog() == DialogResult.OK)
+            {
+                
+                img = (Bitmap)Bitmap.FromFile(open.FileName);
+                width = img.Width;
+                height = img.Height;
+            }
+            if (img.Width % 8 != 0)
+            {
+                width = img.Width + (8 - (img.Width % 8));
+            }
+            if (img.Height % 8 != 0)
+            {
+                height = img.Height + (8 - (img.Height % 8));
+            }
+            img = new Bitmap(img, new Size(width, height));
+            pictureBox1.Image = img;
+            this.img = img;
+        }
+
+        private void CompressBtn_Click(object sender, EventArgs e)
+        {
+            List<Color[,]> eightByEightArray = new List<Color[,]>();
+            
+            int startI = 0;
+            int startJ = 0;
+            int tmpI = 0, tmpJ = 0;
+            while (startJ < img.Height)
+            {
+                Color[,] eightByEight = new Color[8, 8];
+                for (int i = startI; i < startI + 8; i++)
+                {
+                    tmpJ = startJ;
+                    for (int j = startJ; j < startJ + 8; j++)
+                    {
+                        eightByEight[i - startI, j - startJ] = img.GetPixel(i, j);
+                        tmpJ++;
+                    }
+                    tmpI++;
+                }
+                startI = tmpI;
+                eightByEightArray.Add(eightByEight);
+                if (!(tmpI < img.Width))
+                {
+                    startJ = tmpJ;
+                    startI = 0;
+                    tmpI = 0;
+                }
+
+            }
+
+        }
+
+        private void DecompressBtn_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
