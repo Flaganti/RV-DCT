e197739badf4704777688cc124dc9718ba76ed90
Andrej Knez <andrej.531.knez@gmail.com>
2017-01-04 20:25:44 +0100

    dodan FDCT,Cikcak -> ju3 pride not stiskanje pa kompresija

--- RV-DCT/Form1.cs
+++ RV-DCT/Form1.cs
@@ -8,10 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace RV_DCT
 {
+
     public partial class Form1 : Form
     {
+        int[,] tabelaStiskanja = new int[8, 8] { { 15, 14, 13, 12, 11, 10, 9, 8 }, { 14, 13, 12, 11, 10, 9, 8, 7 }, { 13, 12, 11, 10, 9, 8, 7, 6 }, { 12, 11, 10, 9, 8, 7, 6, 5 }, { 11, 10, 9, 8, 7, 6, 5, 4 }, { 10, 9, 8, 7, 6, 5, 4, 3 }, { 9, 8, 7, 6, 5, 4, 3, 2 }, { 8, 7, 6, 5, 4, 3, 2, 1 } };
         Bitmap img;
         public Form1()
         {
@@ -63,9 +66,9 @@ namespace RV_DCT
                     tmpJ = startJ;
                     for (int j = startJ; j < startJ + 8; j++)
                     {
-                        eightByEightR[i - startI, j - startJ] = img.GetPixel(i, j).R;
-                        eightByEightG[i - startI, j - startJ] = img.GetPixel(i, j).G;
-                        eightByEightB[i - startI, j - startJ] = img.GetPixel(i, j).B;
+                        eightByEightR[i - startI, j - startJ] = (byte)(img.GetPixel(i, j).R-128);
+                        eightByEightG[i - startI, j - startJ] = (byte)(img.GetPixel(i, j).G-128);
+                        eightByEightB[i - startI, j - startJ] = (byte)(img.GetPixel(i, j).B-128);
                         tmpJ++;
                     }
                     tmpI++;
@@ -81,12 +84,76 @@ namespace RV_DCT
                     tmpI = 0;
                 }
             }
-
+            int[] newSmth = cikCak(FDCT(eightByEightArrayR[0]));
+            int ss = 9;
+        }
+        public int[,] FDCT(byte[,] eightByEight)
+        {
+            //eightByEight = new byte[8, 8] { { 5, 5, 5, 5, 45, 45, 45, 45 }, { 5, 5, 5, 5, 45, 45, 45, 45 }, { 5, 5, 5, 5, 45, 45, 45, 45 }, { 5, 5, 5, 5, 45, 45, 45, 45 }, { 5, 5, 5, 5, 45, 45, 45, 45 }, { 5, 5, 5, 5, 45, 45, 45, 45 }, { 5, 5, 5, 5, 45, 45, 45, 45 }, { 5, 5, 5, 5, 45, 45, 45, 45 } };
+            int[,] eightByEight_new = new int[8, 8];
+            double c1, c2;
+            double vstota = 0;
+            for(int u = 0; u < 8; u++)
+            {
+                for(int v = 0; v < 8; v++)
+                {
+                    if (u == 0)
+                        c1 = 1.0 / (double)(Math.Sqrt(2));
+                    else
+                        c1 = 1.0;
+                    if(v==0)
+                        c2 = 1.0 / (double)(Math.Sqrt(2));
+                    else
+                        c2 = 1.0;
+                    vstota = 0.0;
+                    for(int x = 0; x < 8; x++)
+                    {
+                        for(int y = 0; y < 8; y++)
+                        {
+                            double CosX = Math.Cos(((2.0 * x + 1.0) * u * Math.PI) / 16.0);
+                            double CosY = Math.Cos(((2.0 * y + 1.0) * v * Math.PI) / 16.0);
+                            vstota += eightByEight[x, y] * CosX * CosY ;
+                        }
+                    }
+                    double one_diVBy_four = 1.0 / 4.0;
+                    eightByEight_new[u, v] = (int)(one_diVBy_four * c1 * c2 * vstota);
+                }
+            }
+            return eightByEight_new;
+        }
+        public int[] cikCak(int[,] array) {
+            int[] cikCak = new int[64];
+            int N = 7;
+            int OVERFLOW = 2033;
+            bool konec = false;
+            int x = 0;
+            int y = 0;
+            int indeks = 0;
+            do
+            {
+                cikCak[indeks] = array[y,x];
+                array[y,x] = OVERFLOW;
+                if ((x > 0) && (y < N) && (array[y + 1,x - 1] < OVERFLOW)) // lahko gre levo dol
+                { x--; y++; }
+                else
+                if ((x < N) && (y > 0) && (array[y - 1,x + 1] < OVERFLOW)) // lahko gre desno gor
+                { x++; y--; }
+                else if ((x > 0) && (x < N)) // lahko gre desno in ni v 1. stolpcu
+                    x++;
+                else if ((y > 0) && (y < N)) // lahko gre dol in ni v 1. vrstici
+                    y++;
+                else if (x < N) // lahko gre desno (in je v 1. stolpcu)
+                    x++;
+                else konec = true;
+                indeks++;
+            } while (konec == false);
+            return cikCak;
         }
 
         private void DecompressBtn_Click(object sender, EventArgs e)
         {
 
         }
+
     }
 }
