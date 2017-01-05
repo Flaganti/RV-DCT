a600fc0da5b7acb07f7ce7f9dd17a23028d40689
Andrej Knez <andrej.531.knez@gmail.com>
2017-01-05 22:02:26 +0100

    brez pisanja v datoteko in brez dekompresije

--- RV-DCT/Form1.cs
+++ RV-DCT/Form1.cs
@@ -16,6 +16,12 @@ namespace RV_DCT
     {
         int[,] tabelaStiskanja = new int[8, 8] { { 15, 14, 13, 12, 11, 10, 9, 8 }, { 14, 13, 12, 11, 10, 9, 8, 7 }, { 13, 12, 11, 10, 9, 8, 7, 6 }, { 12, 11, 10, 9, 8, 7, 6, 5 }, { 11, 10, 9, 8, 7, 6, 5, 4 }, { 10, 9, 8, 7, 6, 5, 4, 3 }, { 9, 8, 7, 6, 5, 4, 3, 2 }, { 8, 7, 6, 5, 4, 3, 2, 1 } };
         Bitmap img;
+        int width = 0, height = 0;
+        int faktorKompresije;
+        List<int[,]> eightByEightArrayR = new List<int[,]>();//vsi 8x8 rdeči barvni kanal
+        List<int[,]> eightByEightArrayG = new List<int[,]>();//vsi 8x8 zeleni barvni kanal
+        List<int[,]> eightByEightArrayB = new List<int[,]>();//vsi 8x8 modri barvni kanal
+
         public Form1()
         {
             InitializeComponent();
@@ -25,7 +31,7 @@ namespace RV_DCT
         {
 
             Bitmap img = null;
-            int width=0, height=0;
+            
             OpenFileDialog open = new OpenFileDialog();
             if(open.ShowDialog() == DialogResult.OK)
             {
@@ -43,40 +49,35 @@ namespace RV_DCT
                 height = img.Height + (8 - (img.Height % 8));
             }
             img = new Bitmap(img, new Size(width, height));
-            pictureBox1.Image = img;
             this.img = img;
-        }
 
-        private void CompressBtn_Click(object sender, EventArgs e)
-        {
-            List<byte[,]> eightByEightArrayR = new List<byte[,]>();//vsi 8x8 rdeči barvni kanal
-            List<byte[,]> eightByEightArrayG = new List<byte[,]>();//vsi 8x8 zeleni barvni kanal
-            List<byte[,]> eightByEightArrayB = new List<byte[,]>();//vsi 8x8 modri barvni kanal
 
+
+            //To sem premakno sem da prihranim čas pri kompresiji
             int startI = 0;//pove kje bomo začeli ko se for zanka začne (kje v sliki smo)
             int startJ = 0;//pove kje bomo začeli ko se for zanka začne (kje v sliki smo)
             int tmpI = 0, tmpJ = 0;//s tem si pomagamo postavljanja trenutne lokacije
             while (startJ < img.Height)//premikamo se od leve proti desni ter vedno nižje. kak stopimp prenisko zaključimo
             {
-                byte[,] eightByEightR = new byte[8, 8];
-                byte[,] eightByEightG = new byte[8, 8];
-                byte[,] eightByEightB = new byte[8, 8];
+                int[,] eightByEightR = new int[8, 8];
+                int[,] eightByEightB = new int[8, 8];
+                int[,] eightByEightG = new int[8, 8];
                 for (int i = startI; i < startI + 8; i++)
                 {
                     tmpJ = startJ;
                     for (int j = startJ; j < startJ + 8; j++)
                     {
-                        eightByEightR[i - startI, j - startJ] = (byte)(img.GetPixel(i, j).R-128);
-                        eightByEightG[i - startI, j - startJ] = (byte)(img.GetPixel(i, j).G-128);
-                        eightByEightB[i - startI, j - startJ] = (byte)(img.GetPixel(i, j).B-128);
+                        eightByEightR[i - startI, j - startJ] = (img.GetPixel(i, j).R - 128);
+                        eightByEightG[i - startI, j - startJ] = (img.GetPixel(i, j).G - 128);
+                        eightByEightB[i - startI, j - startJ] = (img.GetPixel(i, j).B - 128);
                         tmpJ++;
                     }
                     tmpI++;
                 }
                 startI = tmpI;
-                eightByEightArrayR.Add(eightByEightR);
-                eightByEightArrayG.Add(eightByEightG);
-                eightByEightArrayB.Add(eightByEightB);
+                eightByEightArrayR.Add(FDCT(eightByEightR)); //FDCT se najdalje izvaja zato sem ga premakno sem da se naredi preden se slika naloži :v
+                eightByEightArrayG.Add(FDCT(eightByEightG));
+                eightByEightArrayB.Add(FDCT(eightByEightB));
                 if (!(tmpI < img.Width)) //pomaknemo se en 8x8 kvadrat nižje kak pridemo dokonca širine
                 {
                     startJ = tmpJ;
@@ -84,12 +85,34 @@ namespace RV_DCT
                     tmpI = 0;
                 }
             }
-            int[] newSmth = cikCak(FDCT(eightByEightArrayR[0]));
-            int ss = 9;
+            pictureBox1.Image = img;
         }
-        public int[,] FDCT(byte[,] eightByEight)
+
+        private void CompressBtn_Click(object sender, EventArgs e)
         {
-            //eightByEight = new byte[8, 8] { { 5, 5, 5, 5, 45, 45, 45, 45 }, { 5, 5, 5, 5, 45, 45, 45, 45 }, { 5, 5, 5, 5, 45, 45, 45, 45 }, { 5, 5, 5, 5, 45, 45, 45, 45 }, { 5, 5, 5, 5, 45, 45, 45, 45 }, { 5, 5, 5, 5, 45, 45, 45, 45 }, { 5, 5, 5, 5, 45, 45, 45, 45 }, { 5, 5, 5, 5, 45, 45, 45, 45 } };
+            StringBuilder builder = new StringBuilder();
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            for (int i= 0;i < eightByEightArrayR.Count; i++){
+                builder.Append(Code(cikCak(eightByEightArrayR[i])));
+                builder.Append(Code(cikCak(eightByEightArrayG[i])));
+                builder.Append(Code(cikCak(eightByEightArrayB[i])));
+            }
+            watch.Stop();
+            MessageBox.Show((watch.ElapsedMilliseconds).ToString());
+            zapisVDatoteko(builder);
+
+        }
+
+        private void zapisVDatoteko(StringBuilder builder)
+        {
+
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+        }
+
+        public int[,] FDCT(int[,] eightByEight)
+        {
+            //ne pisat tega -> eightByEight = new byte[8, 8] { { 5, 5, 5, 5, 45, 45, 45, 45 }, { 5, 5, 5, 5, 45, 45, 45, 45 }, { 5, 5, 5, 5, 45, 45, 45, 45 }, { 5, 5, 5, 5, 45, 45, 45, 45 }, { 5, 5, 5, 5, 45, 45, 45, 45 }, { 5, 5, 5, 5, 45, 45, 45, 45 }, { 5, 5, 5, 5, 45, 45, 45, 45 }, { 5, 5, 5, 5, 45, 45, 45, 45 } };
             int[,] eightByEight_new = new int[8, 8];
             double c1, c2;
             double vstota = 0;
@@ -97,6 +120,8 @@ namespace RV_DCT
             {
                 for(int v = 0; v < 8; v++)
                 {
+                    if (tabelaStiskanja[u, v] <= faktorKompresije)
+                        continue;
                     if (u == 0)
                         c1 = 1.0 / (double)(Math.Sqrt(2));
                     else
@@ -149,11 +174,86 @@ namespace RV_DCT
             } while (konec == false);
             return cikCak;
         }
+        private String Code(int[] codingArray)
+        {
+            StringBuilder builder = new StringBuilder();
+            if (codingArray[0] < 0) //Prvi se vedno kodira tak! predstavljen je z 11 biti, prvi bit pove ali je + ali - ostalih 10 pa je binarno predstavljeno število
+            {
+                String str = Convert.ToString((codingArray[0] * (-1)), 2);
+                builder.Append(str.PadLeft(10, '0').PadLeft(11, '1'));
+            }
+            else
+            {
+                String str = Convert.ToString((codingArray[0]), 2);
+                builder.Append(str.PadLeft(11, '0'));
+            }
+            int nicle = 0;
+            for (int i = 1; i < 64; i++)
+            {
+
+                if (codingArray[i] == 0) //zaporedje 0 se začne
+                {
+                    nicle++;//stejemo koliko je nicel
+                }
+                else if (codingArray[i] != 0 && codingArray[i - 1] == 0)  // a način
+                {
+                    //zaporedje 0 se konca
+                    int stevilo = codingArray[i];
+                    builder.Append('0');
+                    builder.Append(Convert.ToString(nicle, 2).PadLeft(6, '0'));//v dolžini 6 zapišemo koliko je blo ničel
+                    if (stevilo > 0)
+                    {
+                        String str = Convert.ToString(stevilo, 2);
+                        int dolzina = str.Length + 1; // za predznak
+                        builder.Append(Convert.ToString(dolzina, 2).PadLeft(4, '0'));
+                        builder.Append(str.PadLeft(dolzina, '0')); //dodamo še predznak
+                    }
+                    else
+                    {
+                        String str = Convert.ToString(stevilo * (-1), 2);
+                        int dolzina = str.Length + 1; // za predznak
+                        builder.Append(Convert.ToString(dolzina, 2).PadLeft(4, '0')); //zapišemo v kakšno dolžini je zapisano število
+                        builder.Append(str.PadLeft(dolzina, '1')); //zapišemo število in dodamo še predznak
+                    }
+                    nicle = 0;
+                }
+                else if (codingArray[i] != 0 && codingArray[i - 1] != 0)//način c
+                {
+                    builder.Append('1');//nam pove da gre za c
+                    if (codingArray[i] > 0)
+                    {
+                        String str = Convert.ToString(codingArray[i], 2);
+                        int dolzina = str.Length + 1;
+                        builder.Append(Convert.ToString(dolzina,2).PadLeft(4, '0'));
+                        builder.Append(str.PadLeft(dolzina, '0'));
+                    }
+                    else
+                    {
+                        String str = Convert.ToString(codingArray[i]*(-1), 2);
+                        int dolzina = str.Length + 1;
+                        builder.Append(Convert.ToString(dolzina, 2).PadLeft(4, '0'));
+                        builder.Append(str.PadLeft(dolzina, '1'));
+                    }
+                }
+            }
+            if (nicle > 0)// izvedemo b -> do konca array so se ble same ničle!
+            {
+                builder.Append(0);
+                builder.Append(Convert.ToString(nicle, 2).PadLeft(6, '0'));
+            }
+
+            return builder.ToString();
+        }
 
         private void DecompressBtn_Click(object sender, EventArgs e)
         {
 
         }
 
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            label1.Text = trackBar1.Value.ToString();
+            faktorKompresije = trackBar1.Value;
+        }
     }
 }
